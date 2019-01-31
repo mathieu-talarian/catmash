eb66a1d828fe0b0df6189feef7f8c84966982e1f
Mathieu Moullec <mathieumoullec@MacBook-Pro-de-Mathieu.local>
2019-01-31 13:02:24 +0100

    cleanup

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,53 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import api from '../api'
-import { Link } from "react-router-dom"
-import { Row, Col } from "antd"
-class HomePage extends React.Component {
+import { Link } from 'react-router-dom'
+import { Row, Col } from 'antd'
 
-  state = {
-    loading: false,
-    data: {
-      cat1: {},
-      cat2: {}
-    }
-  }
+const HomePage = () => {
+  const [loading, updateLoading] = useState(false)
+  const [cats, updateCats] = useState({
+    cat1: {},
+    cat2: {}
+  })
 
-  componentDidMount() {
+  const onInitialRender = () => {
+    updateLoading(true)
     api.cats.get()
-      .then(res =>
-        this.setState({ ...this.state, data: res }))
+      .then(res => {
+        updateCats(res)
+        updateLoading(false)
+      })
       .catch(err => console.log(err))
   }
 
-  onSubmit = (e, res) => {
+  const onSubmit = (e, res) => {
     const { cat1, cat2 } = this.state.data
     e.preventDefault()
     this.setState({ loading: true })
     api.vote.post({
-      cat1: { id: cat1.ID, voted: res === 1 ? true : false },
-      cat2: { id: cat2.ID, voted: res === 2 ? true : false }
+      cat1: { id: cat1.ID, voted: res === 1 },
+      cat2: { id: cat2.ID, voted: res === 2 }
     })
       .then()
       .catch(err => console.log(err))
   }
 
-  render() {
-    const { data } = this.state
-    return (
-      <div>
-        HomePage
-        <Row type="flex">
-          <Col span={12}>
-            <img onClick={(e) => this.onSubmit(e, 1)} alt="cat1" src={data.cat1.image} />
-          </Col>
-          <Col span={12}>
-            <img onClick={(e) => this.onSubmit(e, 2)} alt="cat2" src={data.cat2.image} />
-          </Col>
-        </Row>
-        <Link to="/results">Resultats</Link>
-      </div>)
-  }
+  useEffect(onInitialRender, [])
+
+  return (
+    <div>
+      HomePage
+      <Row type="flex">
+        <Col span={12}>
+          <img onClick={(e) => onSubmit(e, 1)} alt="cat1" src={cats.cat1.image}/>
+        </Col>
+        <Col span={12}>
+          <img onClick={(e) => onSubmit(e, 2)} alt="cat2" src={cats.cat2.image}/>
+        </Col>
+      </Row>
+      <Link to="/results">Resultats</Link>
+    </div>)
 }
 
 HomePage.propTypes = {
