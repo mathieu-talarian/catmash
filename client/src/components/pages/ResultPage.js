eadad4300e6941f36322bd46b377c3caed5e3d96
Mathieu Moullec <mathieumoullec@MacBook-Pro-de-Mathieu.local>
2019-02-05 00:54:45 +0100

    rating redo

--- client/src/components/pages/ResultPage.js
+++ client/src/components/pages/ResultPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Table, Avatar } from "antd";
+import { Table, Avatar, Rate, Icon } from "antd";
 import { toast, ToastContainer } from "react-toastify";
 import api from "../../api";
 import toastOptions from "../tools/toastOptions";
@@ -12,31 +12,10 @@ import toastOptions from "../tools/toastOptions";
 }
 */
 
-const columns = [
-  {
-    title: "Image",
-    key: "image",
-    dataIndex: "image",
-    render: text => {
-      return <Avatar shape="square" size={64} src={text} />;
-    },
-    align: "center"
-  },
-  {
-    title: "Nombre de votes",
-    dataIndex: "votes",
-    key: "votes"
-  },
-  {
-    title: "Rating",
-    dataIndex: "rating",
-    key: "rating"
-  }
-];
-
 const ResultPage = () => {
   const [cats, updateCats] = useState([]);
   const [loading, updateLoading] = useState(false);
+  const [max, updateMax] = useState(0);
 
   const onInitialRender = () => {
     updateLoading(true);
@@ -44,17 +23,53 @@ const ResultPage = () => {
       .getAllOrdered()
       .then(res => {
         updateLoading(false);
+        updateMax(res[0].rating);
         updateCats(res);
       })
-      .catch(() => toast.error("un probleme est survenu avec le serveur"));
+      .catch(err =>
+        toast.error(`un probleme est survenu avec le serveur ${err}`)
+      );
   };
 
   useEffect(onInitialRender, []);
+
+  const columns = [
+    {
+      title: "Image",
+      key: "image",
+      dataIndex: "image",
+      render: text => <Avatar shape="square" size={64} src={text} />,
+      align: "center"
+    },
+    {
+      title: "Nombre de votes",
+      dataIndex: "votes",
+      key: "votes"
+    },
+    {
+      title: "Rating",
+      dataIndex: "rating",
+      key: "rating",
+      render: text => {
+        const value = (text / max) * 5.0;
+        return (
+          <Rate
+            character={<Icon type="heart" />}
+            value={value}
+            allowHalf
+            disabled
+            style={{ color: "red" }}
+          />
+        );
+      }
+    }
+  ];
 
   return (
     <div>
       <ToastContainer {...toastOptions} />
       <Table
+        max={max}
         rowKey={record => record.ID}
         key={record => record.ID}
         loading={loading}
